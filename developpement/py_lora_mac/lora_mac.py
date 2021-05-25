27d90af640740498d9bd57266a3ceb5cac4e23db
ArnaudPalgen <arnaud.palgen@student.umons.ac.be>
2021-05-25 18:53:38 +0200

    new py mac to handler multiple childs at the same time

--- developpement/py_lora_mac/lora_mac.py
+++ developpement/py_lora_mac/lora_mac.py
@@ -1,6 +1,8 @@
 import queue
-import threading
+from threading import Timer, Event, Thread
 from lora_phy import LoraAddr, LoraFrame, LoraPhy, MacCommand
+from dataclasses import dataclass
+
 
 MAX_PREFIX = 0xFC#252
 MIN_PREFIX = 2
@@ -9,20 +11,31 @@ ROOT_PREFIX = 1
 ROOT_ID = 0
 
 RETRANSMIT_TIMEOUT = 5#sec
+MAX_RETRANSMIT = 3
 
+CHILD_TX_BUF_SIZE = 5
+CHILD_QUEUE_SIZE = 10
+
+
+class LoraChild():
+    def __init__(self, addr:LoraAddr, retransmit_fun:callable):
+        self.addr = addr
+        self.ack = False
+        self.timer = Timer(RETRANSMIT_TIMEOUT, retransmit_fun, [self])
+        self.last_send_frame = None
+        self.transmit_count = 0
+        self.can_send = True
+        self.tx_buf = queue.Queue(CHILD_TX_BUF_SIZE)
+        self.lost_packet = 0
 
 class LoraMac():
     def __init__(self):
         self.phy_layer = LoraPhy()
-        self.childs = []
+        self.childs = {}
         self.addr = LoraAddr(ROOT_PREFIX,ROOT_ID)
         self.action_matcher = {MacCommand.JOIN:self._on_join, MacCommand.ACK:self._on_ack}
-        self.tx_buf = queue.Queue(10)
-        self.retransmit_timer = threading.Timer(RETRANSMIT_TIMEOUT, self._retransmit_timeout)
-        self.last_send_frame = None
-        self.can_send = True
-        self.expected_ack = False
-        self.can_send_cond = threading.Condition()
+        self.last_prefix = -1
+        self.childs_buf=queue.Queue(CHILD_QUEUE_SIZE)
 
     def mac_init(self):
         self.phy_layer.phy_register_listener(self._mac_rx)
@@ -30,58 +43,80 @@ class LoraMac():
         self.phy_layer.phy_timeout(0)
         self.phy_layer.phy_rx()
 
-        tx_thread = threading.Thread(target=self._tx_process)
+        tx_thread = Thread(target=self._tx_process)
         tx_thread.start()
 
-        with self.can_send_cond:
-            self.can_send_cond.notify_all()
 
-    def _retransmit_timeout(self):
-        print("retransmit timeout")
+    def _retransmit_timeout(self, child):
+        print("retransmit timeout:", child)
+        if child.transmit_count >= MAX_RETRANSMIT:
+            print("unable to send frame", child.last_send_frame)
+            child.lost_packet +=1
+            print("lost packets:" ,child.lost_packet)
+            child.ack = not child.ack
+            child.last_send_frame = None
+            child.transmit_count = 0
+            child.can_send = True
+            self.childs_buf.put(child)
+
+        else:
+            child.transmit_count +=1
+            self.phy_layer.phy_tx(child.last_send_frame)
+            #child.timer.start()
     
     def _on_ack(self, frame:LoraFrame):
-        if frame.payload == self.expected_ack:
-            with self.can_send_cond:
-                self.can_send = True
-                self.can_send_cond.notify_all()
+        oldAddr = LoraAddr(0,frame.src_addr.node_id)
+        child = self.childs[oldAddr]
+        if frame.seq == child.ack:
+            if child.last_send_frame.command==MacCommand.JOIN_RESPONSE:
+                child.addr=LoraAddr(int(child.last_send_frame.payload, 16), frame.dest_addr.node_id)
+                self.childs[child.addr] = child
+                del self.childs[oldAddr]
+            child.ack = not child.ack
+            child.timer.cancel()
+            child.can_send = True
+            self.childs_buf.put(child)
 
     def _on_join(self, frame:LoraFrame):
         print("enter on_join")
         if len(self.childs) == 0:
-            new_child = MIN_PREFIX
-            print("len(child)=0")
-        elif len(self.childs) > 0 and self.childs[-1].prefix < MAX_PREFIX:
-            print("len(child)>0")
-            new_child = self.childs[-1].prefix+1
+            new_prefix = MIN_PREFIX
+        elif len(self.childs) > 0 and self.last_prefix < MAX_PREFIX:
+            new_prefix = self.last_prefix+1
         
-        new_child_addr = LoraAddr(new_child, frame.src_addr.node_id)
+        new_child_addr = LoraAddr(0, frame.src_addr.node_id)
+        new_child = LoraChild(new_child_addr, self._retransmit_timeout)
+        
+        self.childs[new_child_addr] = new_child
         print("new child:", new_child_addr)
-        self.childs.append(new_child_addr)
         
         print("send join response")
-        self.mac_tx(LoraFrame(self.addr, frame.src_addr, True, MacCommand.JOIN_RESPONSE, "%02X"%new_child))
+        self.mac_tx(LoraFrame(self.addr, frame.src_addr, True, MacCommand.JOIN_RESPONSE, "%02X"%new_prefix))#todo ack?
 
     def mac_tx(self, frame: LoraFrame)->bool:
-        print("in mac tx. FULL:", self.tx_buf.full())
-        if not self.tx_buf.full():
-            self.tx_buf.put(frame, block=False)
-            print("putted:", self.tx_buf)
+        child = self.childs[frame.dest_addr]
+        print("in mac tx. FULL:", child.tx_buf.full())
+        if not child.tx_buf.full():
+            child.tx_buf.put(frame, block=False)
+            print("putted for :", child, child.tx_buf)
+            if child.can_send:
+                self.childs_buf.put(child)
             return True
         return False
-
     
     def _tx_process(self):
         while True:
-            while not self.can_send:
-                with self.can_send_cond:
-                    self.can_send_cond.wait()
-            self.last_send_frame = self.tx_buf.get(block=True)
-            self.phy_layer.phy_tx(self.last_send_frame)
-            print(self.last_send_frame, "sended to phy")
-            if self.last_send_frame.k:
-                self.retransmit_timer.start()
-                self.expected_ack = not self.expected_ack
-                self.can_send = False
+            child = self.childs_buf.get()
+            while child.can_send and child.tx_buf.qsize()>0:
+                try:
+                    frame = child.tx_buf.get(block=False)
+                except queue.Empty:
+                    break
+                child.last_send_frame = frame
+                self.phy_layer.phy_tx(frame)
+                if frame.k:
+                    child.timer.start()
+                    self.can_send=False
 
     def _mac_rx(self, frame:LoraFrame):
         print("MAC RX:", frame)
