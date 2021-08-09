498c18ab934ef7b0a78ed48e5bc637ecf3e9f719
ArnaudPalgen <arnaud.palgen@student.umons.ac.be>
2021-08-09 09:30:14 +0200

    remove build dir ping pong example

--- developpement/ping_pong_example/contiki_ping_pong/build/zoul/remote-revb/obj/crc16.d
+++
@@ -1,5 +0,0 @@
-build/zoul/remote-revb/obj/crc16.o: \
- /home/arnaud/contiki-ng/os/lib/crc16.c \
- /home/arnaud/contiki-ng/os/services/loramac/module-macros.h
- /home/arnaud/contiki-ng/os/lib/crc16.c :
- /home/arnaud/contiki-ng/os/services/loramac/module-macros.h :
